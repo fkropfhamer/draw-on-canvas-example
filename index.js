e0fe85729c607d2aedb47aa4e23097d16240d136
fabian <fabian.kropfhamer@gmail.com>
2020-09-03 21:02:32 +0200

    added reset and download button

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import Draw from 'draw-on-canvas';
 function main() {
     console.log('test');
     const el = document.getElementById('draw');
-    new Draw(el, 256, 256, {backgroundColor: 'black', strokeColor: 'white', strokeWeight: 20});
+    const d = new Draw(el, 256, 256, {backgroundColor: 'black', strokeColor: 'white', strokeWeight: 20});
+
+    const resetButton = document.getElementById('reset');
+    resetButton.onclick = () => d.reset();
+
+    const downloadButton = document.getElementById('download');
+    downloadButton.onclick = () => d.donwloadPNG();
 }
 
 main();
