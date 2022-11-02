fbe8c57bdd35314b51ace6d09218d192caa14e1e
Fabian Kropfhamer <fabian.kropfhamer@gmail.com>
2022-11-02 07:15:36 +0100

    update example.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import Draw from 'draw-on-canvas';
 function main() {
     console.log('test');
     const el = document.getElementById('draw');
-    const d = new Draw(el, 256, 256, {backgroundColor: 'black', strokeColor: 'white', strokeWeight: 20});
+    const d = new Draw(el, 256, 256, { style: { borderStyle: 'solid', borderColor: 'yellow' }, backgroundColor: 'black', strokeColor: 'white', strokeWeight: 20});
 
     const resetButton = document.getElementById('reset');
     resetButton.onclick = () => d.reset();
